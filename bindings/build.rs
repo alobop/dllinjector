4fc969cf88126775190497739f2763e58012d142
Alejandro Lobo <alobop@outlook.com>
2021-06-15 08:48:43 -0700

    Add injector logic

--- bindings/build.rs
+++ bindings/build.rs
@@ -1,20 +1,22 @@
 fn main() {
     windows::build!(
         // Common
-        Windows::Win32::System::SystemServices::{BOOL, HANDLE, PSTR, HINSTANCE, INVALID_HANDLE_VALUE, NULL, HINSTANCE},
+        Windows::Win32::System::WindowsProgramming::{INFINITE},
+        Windows::Win32::Foundation::CloseHandle,
+        Windows::Win32::Foundation::{ BOOL, HANDLE, PSTR, HINSTANCE, INVALID_HANDLE_VALUE, HINSTANCE},
         // Thread
         Windows::Win32::System::SystemServices::LPTHREAD_START_ROUTINE,
         Windows::Win32::System::Threading::CreateRemoteThread,
         // Security
-        Windows::Win32::System::SystemServices::SECURITY_ATTRIBUTES,
+        Windows::Win32::Security::SECURITY_ATTRIBUTES,
         // Process
-        Windows::Win32::System::Threading::{GetCurrentProcessId, OpenProcess, PROCESS_ALL_ACCESS, PROCESS_QUERY_INFORMATION, PROCESS_VM_READ},
-        Windows::Win32::System::SystemServices::{GetModuleHandleA, GetProcAddress, FARPROC},
+        Windows::Win32::System::Threading::{WaitForSingleObject, GetCurrentProcessId, OpenProcess, PROCESS_ACCESS_RIGHTS},
+        Windows::Win32::System::LibraryLoader::{GetModuleHandleA, GetProcAddress},
         Windows::Win32::System::ProcessStatus::{K32EnumProcesses, K32GetModuleFileNameExA},
         // Memory
         Windows::Win32::System::Diagnostics::Debug::WriteProcessMemory,
-        Windows::Win32::System::Memory::{VirtualAllocEx, MEM_RESERVE, MEM_COMMIT},
-        Windows::Win32::System::SystemServices::PAGE_EXECUTE_READWRITE,
+        Windows::Win32::System::Memory::{VirtualAllocEx, VIRTUAL_ALLOCATION_TYPE},
+        Windows::Win32::System::Memory::PAGE_TYPE,
         // I/O
         Windows::Win32::Storage::FileSystem::GetFullPathNameA,
     );
--- src/main.rs
+++ src/main.rs
@@ -1,46 +1,107 @@
+#![allow(unused_parens)]
+
 use bindings::Windows::Win32::System::ProcessStatus::*;
-use bindings::Windows::Win32::System::SystemServices::*;
+use bindings::Windows::Win32::Foundation::*;
 use bindings::Windows::Win32::System::Threading::*;
+use bindings::Windows::Win32::System::Memory::*;
+use bindings::Windows::Win32::System::Diagnostics::Debug::*;
+use bindings::Windows::Win32::System::LibraryLoader::*;
+use bindings::Windows::Win32::System::WindowsProgramming::*;
 
-use std::convert::TryInto;
+use std::{convert::TryInto, u32};
 use std::str;
+use std::env;
+use core::ffi::c_void;
 
-fn main() {
+fn enumerate_processes()
+{
     let mut processes: Vec<u32> = Vec::new();
     processes.resize(300, 0);
     let mut count: u32 = 0;
-    let result_bool;
     let size: u32 = (processes.len() * std::mem::size_of::<i32>())
         .try_into()
         .unwrap();
 
     unsafe {
-        let result: BOOL = K32EnumProcesses(processes.as_mut_ptr(), size, &mut count);
-        result_bool = bool::from(result);
+        let _result: BOOL = K32EnumProcesses(processes.as_mut_ptr(), size, &mut count);
     }
 
-    println!("Retrieved processes {}, count {}", result_bool, count / 4);
-    println!("{:?}", processes);
-
     processes.iter().for_each(|item| {
-        let handle: HANDLE;
 
-        unsafe {
-            handle = OpenProcess(PROCESS_QUERY_INFORMATION | PROCESS_VM_READ, false, *item);
+        if (*item != 0)
+        {
+            let handle: HANDLE;
 
-            let name = if handle.is_null() {
-                String::from("No name")
-            } else {
-                let mut name_vec = vec![0u8; 100];
-                let name_ptr = name_vec.as_mut_ptr();
-                let name = PSTR { 0: name_ptr };
-                let a = K32GetModuleFileNameExA(handle, HINSTANCE::default(), name, 100);
+            unsafe {
+                handle = OpenProcess(PROCESS_QUERY_INFORMATION | PROCESS_VM_READ, false, *item);
 
-                let s = str::from_utf8(&name_vec).unwrap();
-                String::from(s)
-            };
+                let name = if handle.is_null() {
+                    String::from("No name")
+                } else {
+                    let mut name_vec = vec![0u8; 100];
+                    let name_ptr = name_vec.as_mut_ptr();
+                    let name = PSTR { 0: name_ptr };
+                    let _a = K32GetModuleFileNameExA(handle, HINSTANCE::default(), name, 100);
+                    CloseHandle(handle);
+                    let s = str::from_utf8(&name_vec).unwrap();
+                    String::from(s)
+                    
+                };
 
-            println!("{} {}", item, name);
+                println!("{} {}", item, name);
+            }
         }
     });
+}
+
+fn inject_dll(id:u32)
+{
+    let process_handle =  unsafe{
+        OpenProcess(PROCESS_ALL_ACCESS, false, id)
+    };
+
+    if (process_handle.is_null())
+    {
+        println!("Unable to open {}", id);
+        return
+    }
+
+    else
+    {
+        println!("Opened process");
+    }
+
+    let path = "E:\\injecteddll\\target\\debug\\injecteddll.dll";
+
+    let dll_path_address = unsafe{VirtualAllocEx(process_handle, std::ptr::null_mut(), path.len() + 1, MEM_COMMIT, PAGE_READWRITE)};
+
+    println!("Address {:?}", dll_path_address);
+
+    let mut bytes_writen: usize = 0;
+    let result = unsafe{WriteProcessMemory(process_handle, dll_path_address, path.as_ptr() as *const c_void, path.len() + 1, &mut bytes_writen)};
+
+    println!("Wrote into memory {}: {}", bool::from(result), bytes_writen);
+
+    let kernel32_module = unsafe{GetModuleHandleA("Kernel32.dll")};
+    let proc_address = unsafe{GetProcAddress(kernel32_module, "LoadLibraryA")};
+    let thread_handle = unsafe{CreateRemoteThread(process_handle, std::ptr::null_mut(), 0, Some(std::mem::transmute(proc_address)), dll_path_address, 0, std::ptr::null_mut())};
+
+    println!("Thread handle {:?}", thread_handle);
+
+    unsafe{WaitForSingleObject(thread_handle, INFINITE)};
+
+    println!("DLL loaded");
+    unsafe {
+        CloseHandle(process_handle);
+    }
+    
+}
+
+fn main() {
+    let process_number = env::args().nth(1);
+
+    match process_number {
+        Some(pid) => inject_dll(pid.parse().unwrap()),
+        None => enumerate_processes()
+    }
 }
