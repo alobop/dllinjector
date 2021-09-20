ee0ba614d7c326505736dccfa104b8860049ff0a
Asher Davila <adavila@predator.local>
2021-09-19 22:04:53 -0700

    Made changes as requested

--- bindings/build.rs
+++ bindings/build.rs
@@ -3,9 +3,9 @@ fn main() {
         // Common
         Windows::Win32::System::WindowsProgramming::{INFINITE},
         Windows::Win32::Foundation::CloseHandle,
-        Windows::Win32::Foundation::{ BOOL, HANDLE, PSTR, HINSTANCE, INVALID_HANDLE_VALUE, HINSTANCE, FARPROC},
+        Windows::Win32::Foundation::{ BOOL, HANDLE, PSTR, HINSTANCE, INVALID_HANDLE_VALUE, HINSTANCE, FARPROC, MAX_PATH},
         // Thread
-        Windows::Win32::System::SystemServices::LPTHREAD_START_ROUTINE,
+        Windows::Win32::System::SystemServices::{DLL_PROCESS_ATTACH, DLL_PROCESS_DETACH, DLL_THREAD_ATTACH, DLL_THREAD_DETACH,LPTHREAD_START_ROUTINE},
         Windows::Win32::System::Threading::{CreateRemoteThread, WAIT_RETURN_CAUSE},
         // Security
         Windows::Win32::Security::SECURITY_ATTRIBUTES,
--- injecteddll/src/lib.rs
+++ injecteddll/src/lib.rs
@@ -1,9 +1,9 @@
 // lib.rs
 
-use bindings_dll::Windows::Win32::System::Threading::*;
-use bindings_dll::Windows::Win32::Foundation::*;
-use bindings_dll::Windows::Win32::System::ProcessStatus::{K32EnumProcessModules, K32GetModuleFileNameExA};
-use bindings_dll::Windows::Win32::System::SystemServices::*;
+use bindings::Windows::Win32::System::Threading::*;
+use bindings::Windows::Win32::Foundation::*;
+use bindings::Windows::Win32::System::ProcessStatus::{K32EnumProcessModules, K32GetModuleFileNameExA};
+use bindings::Windows::Win32::System::SystemServices::*;
 
 use core::ffi::c_void;
 use std::convert::TryInto;
@@ -11,7 +11,7 @@ use std::mem;
 use std::u32;
 use std::str;
 use std::fmt;
-use winsafe_dll::{winexec, SafeHandle};
+use winsafe::{winexec, SafeHandle};
 
 #[derive(Debug)]
 pub struct Process {
@@ -30,7 +30,7 @@ impl fmt::Display for Process {
 pub extern "stdcall" fn DllMain(_handle: HINSTANCE, reason: u32, _ptr:*mut c_void) -> bool {
 
     match reason{
-        DLL_PROCESS_ATTACH =>  enumerate_modules(),
+        DLL_PROCESS_ATTACH =>  {enumerate_modules().unwrap()},
         DLL_PROCESS_DETACH => println!("Injected the code pd"),
         DLL_THREAD_ATTACH => println!("Injected the code ta"),
         DLL_THREAD_DETACH => println!("Injected the code td"),
@@ -40,25 +40,18 @@ pub extern "stdcall" fn DllMain(_handle: HINSTANCE, reason: u32, _ptr:*mut c_voi
     return true;
 }
 
-pub fn enumerate_modules()
+pub fn enumerate_modules()-> winsafe::Result<()>
 {
     let mut h_mods: [HINSTANCE; 1024] = [HINSTANCE::default(); 1024];
     let mut cb_needed: u32 = 0;
 
     let mut i = 0;
     
-    let id: u32 = unsafe{ GetCurrentProcessId()};
+    let id = winexec!(GetCurrentProcessId())?;
     println!("Current process ID: {}", id);
+    
 
     if let Ok(h_process) = winexec!(SafeHandle::from(OpenProcess( PROCESS_QUERY_INFORMATION | PROCESS_VM_READ, false, id ))){
-
-        // let _name = if h_process.is_null()
-        // {
-        //     String::from("No process")
-
-        // } else{
-        //     String::from("We are good")
-        // };
 
         let size_h_mods:  u32 = (h_mods.len() * mem::size_of::<HINSTANCE>())
             .try_into()
@@ -77,7 +70,7 @@ pub fn enumerate_modules()
                 let mut name_vec = vec![0u8; 100];
                 let name_ptr = name_vec.as_mut_ptr();
                 let name = PSTR { 0: name_ptr };
-                //let module_file_result = unsafe {  K32GetModuleFileNameExA( h_process, h_mods[i as usize], name, MAX_PATH ) } ;
+
                 if let Ok(_module_file_result) = winexec!(K32GetModuleFileNameExA( h_process.handle, h_mods[i as usize], name, MAX_PATH ))
                 {
                     let s = str::from_utf8(&name_vec).unwrap();
@@ -90,5 +83,7 @@ pub fn enumerate_modules()
             }
         }
     }
+
+    Ok(())
 
 }
